7f9c332def6a0b7dc62b0b9035d3003b695bb2da
Matz <matzlarsson94@gmail.com>
2021-07-24 23:37:17 +0200

    Add possibility to use custom branch

--- run_latest_webserver.sh
+++ run_latest_webserver.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [ -z "$BADGERCAM_BRANCH" ]
+then
+BADGERCAM_BRANCH="main"
+echo Using default branch $BADGERCAM_BRANCH
+else
+echo Using custom branch $BADGERCAM_BRANCH
+fi
+
+git checkout $BADGERCAM_BRANCH
 git pull
 cd src/web
 npm install
